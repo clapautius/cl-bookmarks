0bf5fdacf1927de42f580b0ea4af19e10543c697
Tudor M. Pristavu <clapautius@gmail.com>
2011-10-05 00:59:35 +0300

    Working on adding bookmarks to sqlite db.

--- firefox.lisp
+++ firefox.lisp
@@ -1,6 +1,7 @@
-;;; functions handling firefox bookmarks (requires cl-sql-odbc & co.)
+;;; functions handling firefox bookmarks (requires cl-sql, cl-sql-sqlite3 & co.)
 
-(require 'clsql)
+;;; :fixme: - add proper package management
+(asdf:oos 'asdf:load-op :clsql-sqlite3)
 
 
 ;;; bookmark class - browser independent
@@ -50,9 +51,8 @@
 		  (cond
 			((equal type-of-elt :bookmark)
 			 (setf bookmark (make-instance 'bookmark :title (second result)
-										   :url (third result)))
-			 ;;(format t "new obj: ~a~%" bookmark))
-			((eq type-of-elt :tag)
+										   :url (third result))))
+			((eql type-of-elt :tag)
 			 (if (null tags)
 				 (setf tags (list (bookm-get-title-of-elt parent-id)))
 				 (nconc tags (list (bookm-get-title-of-elt parent-id)))))))))
@@ -78,3 +78,13 @@
 							id))
 		 (title (car (car (clsql:query query-str :field-names nil)))))
 	title))
+
+(defun bookm-add (bookmark)
+  "Add a bookmark into the firefox sqlite file"
+  (clsql:insert-records :into "moz_places" :attributes '(url title) :values
+						(list (url bookmark) (title bookmark)))
+  (let* ((query-str (format nil "select last_insert_rowid() from moz_places"))
+		 (place-id (car (car (clsql:query query-str :field-names nil)))))
+	(clsql:insert-records :into "moz_bookmarks"
+						  :attributes '(type fk parent title)
+						  :values (list 1 place-id 5 (title bookmark)))))
