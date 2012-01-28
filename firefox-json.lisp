5a039dc5fd918818142ea89a2ca4792184a9e406
Tudor M. Pristavu <clapautius@gmail.com>
2012-01-28 13:36:39 +0200

    Implement functions for bookmarks hash table.

--- firefox-json.lisp
+++ firefox-json.lisp
@@ -1,6 +1,42 @@
 ;;; functions handling firefox bookmarks from json files
 (in-package :cl-bookmarks)
 
+;; all-bookmarks is a hash table for bookmark objects. The key is the uri.
+(let ((all-bookmarks (make-hash-table :test 'equal)))
+
+  (defun add-or-update-bookm (uri title &optional tag)
+    "Add or update a bookmark in all-bookmarks hash table"
+    (let ((existing-bookm (gethash uri all-bookmarks)))
+      (if existing-bookm
+          (progn
+            ;; update title if empty
+            (when (zerop (length (title existing-bookm)))
+              (setf (title existing-bookm) title))
+            ;; add tag
+            (when (not (bookm-has-tag-p existing-bookm tag))
+              (bookm-add-tag existing-bookm tag)))
+          (progn
+            (setf (gethash uri all-bookmarks) 
+                  (make-instance 'bookmark :url uri :title title
+                                 :tags (list tag)))))))
+
+  (defun get-bookm-sorted-by-uri ()
+    "Return a list of bookmarks sorted alphabetically by uri"
+    (let ((bookm-list (list 0))) ;; use a fake first elt
+      (maphash (lambda (key value) (declare (ignore key))
+                       (nconc bookm-list (list value)))
+               all-bookmarks)
+      (setf bookm-list (cdr bookm-list)) ;; skip the fake first elt
+      (sort bookm-list (lambda (a b) (string< (url a) (url b))))))
+
+  (defun get-all-bookm ()
+    "Return a list with all bookmarks (unsorted)"
+    (let ((bookm-list (list 0))) ;; use a fake first elt
+      (maphash (lambda (key value) (declare (ignore key))
+                       (nconc bookm-list (list value)))
+               all-bookmarks)
+      (cdr bookm-list))))
+      
 
 (defun do-json-obj (obj)
   "Analyze a json object"
